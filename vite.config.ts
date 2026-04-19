c35f11881e3b6227388ee3ba83993d91cfedf5c2
Othmane <o_frds@outlook.com>
2026-04-19 13:12:56 -0400

    Vercel fix v2

--- vite.config.ts
+++ vite.config.ts
@@ -7,10 +7,5 @@
 import { defineConfig } from "@lovable.dev/vite-tanstack-config";
 
 export default defineConfig({
-  cloudflare: false,
-  tanstackStart: {
-    server: {
-      preset: 'vercel'
-    }
-  }
+  cloudflare: false
 });
